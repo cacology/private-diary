68f215e9dd7c85f3d2ed73d63c3fa4c176ee93ba
Syohei YOSHIDA <syohex@gmail.com>
2015-12-16 07:46:13 +0900

    Remove epg from dependency list
    Because it was bundled since Emacs 24.

--- private-diary.el
+++ private-diary.el
@@ -6,7 +6,7 @@
 ;; Keywords: diary, encryption
 ;; URL: https://github.com/cacology/private-diary
 ;; Version: 1.0
-;; Package-Requires: ((emacs "24.0") (epg))
+;; Package-Requires: ((emacs "24.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
